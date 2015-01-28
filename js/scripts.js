4c84e30408361df218744f57ecc20e46b2842ca4
Eduardo Matos <eduardoj.matos@gmail.com>
2015-01-28 17:51:36 -0200

    Tratamento de erro no envio do formulário e mensagens de sucesso/falha no subscribe

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
-    $('#submit').on('click', function() {
-        $.ajax({
-            crossDomain: true,
-            datatype: 'jsonp',
-            type: 'post',
-            url: 'add_subscriber.php',
-            data: $('#formEmail').serialize(),
-            success: function(responseData) {
-                debugger;
-            },  
-            error: function(responseData) {
-                debugger;
-            }
-        });
+
+  $('#submit').on('click', function() {
+
+    var $form = $('#formEmail');
+
+    $.ajax({
+      crossDomain: true,
+      datatype: 'jsonp',
+      type: 'POST',
+      url: 'http://2014.staging.agilebrazil.com/add_subscriber.php',
+      data: $form.serialize(),
+      success: function( response, status ) {
+        var jsonResponse = JSON.parse(response) || { "status": "error" };
+
+        if ( jsonResponse.status === 'error' ) {
+          $('.success', $form).addClass('hide');
+          $('.error', $form).removeClass('hide');
+          return false;
+        }
+
+        $('.error', $form).addClass('hide');
+        $('.success', $form).removeClass('hide');
+      },
+      error: function( xhr, status ) {
+        $('.error', $form).removeClass('hide');
+      }
     });
+  });
 });
