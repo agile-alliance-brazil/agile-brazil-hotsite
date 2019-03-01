db410da34b5df7f551865e0b60709af167448330
Stéphano Wallace <lastephano.w@Stephanos-MacBook-Pro.local>
2019-03-01 19:17:20 -0300

    add Footer component

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -1,16 +1,36 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 const React = require('react');
 
-class Footer extends React.Component {
-  render() {
-    return null;
-  }
-}
+const Footer = props => (
+  <footer className="nav-footer" id="footer">
+    <section className="sitemap">
+      <div>
+        <h5>Redes Sociais</h5>
+        <a href="https://www.facebook.com/AgileBrazil" target="_blank">
+          Facebook
+        </a>
+        <a href="https://www.linkedin.com/groups/Agile-Brazil-3148432/" target="_blank">
+          LinkedIn
+        </a>
+        <a href="https://twitter.com/agilebrazil" target="_blank">
+          Twitter
+        </a>
+        <a href="https://vimeo.com/agilebrazil" target="_blank">
+          Vimeo
+        </a>
+      </div>
+      <div>
+        <h5>Contato</h5>
+        <a href="mailto:contato@agilebrazil.com" target="_blank">
+          contato@agilebrazil.com
+        </a>
+      </div>
+      <img
+        src={`${props.config.baseUrl}img/logoAgileBrazil.png`}
+        alt="Logo Agile Brazil"
+        height="45"
+      />
+    </section>
+  </footer>
+);
 
 module.exports = Footer;
