35ccdb21145a61e3d705f26e6c6eb8472212fc59
Ayrton Araujo <ayr-ton@users.noreply.github.com>
2019-03-18 22:42:28 -0300

    Prod settings

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -1,31 +1,9 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
 const siteConfig = {
   title: 'Agile Brazil', 
   city: 'Belo Horizonte',
   year: '2019',
   tagline: 'Belo Horizonte, 2019',
-  url: 'http://staging.agilebrazil.com', 
+  url: 'https://www.agilebrazil.com', 
   baseUrl: '/2019/', 
   projectName: 'agile-brazil-hotsite',
   organizationName: 'agile-alliance-brazil',
@@ -36,7 +14,6 @@ const siteConfig = {
     { languages: true }
   ],
   headerIcon: 'img/logo-agile-brazil.svg',
-  footerIcon: 'img/docusaurus.svg',
   favicon: 'img/favicon.png',
   colors: {
     primaryColor: '#5C95B6',
@@ -49,8 +26,6 @@ const siteConfig = {
   scripts: ['https://buttons.github.io/buttons.js'],
   onPageNav: 'separate',
   cleanUrl: true,
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
   gaTrackingId: 'UA-134176117-1',
   gaGtag: true,
   enableUpdateTime: true,
