a20b43a640657c8654c9cbe7a365c855e85b22c7
Weslley Alves <wesshead@gmail.com>
2019-03-28 09:07:49 -0300

    Criado a estrutura com os evento do programa

--- website/pages/en/program.js
+++ website/pages/en/program.js
@@ -104,14 +104,192 @@ class Program extends React.Component {
       </section>
     );
 
+    const TimeLineEvents = () => (
+      <section className="program--timeline-events">
+        <h2>Antes de abrir submissões...</h2>
+        <ul>
+          <li class="chairs">
+            <h3>Convite do comitê</h3>
+            <div>
+              <p>Pessoas que compuseram o comitê de Programa no ano anterior, revisores que se destacaram e participantes que demonstraram bastante interesse em ajudar no Programa da próxima conferência são convidados para compor o comitê de Programa.</p>
+            </div>
+          </li>
+          <li class="committee">
+            <h3>Levantamento de candidatos a keynote</h3>
+            <div>
+              <p>Todo ano, levantamos nomes de pessoas que poderiam ser keynote speakers no ano corrente, considerando o público esperado e a presença de palco.</p>
+
+              <p>São 2 keynote speakers internacionais, 1 nacional e 1 keynote speaker fora-da-caixa - este último para falar de assuntos não-relacionado à agilidade.</p>
+            </div>
+          </li>
+          <li class="chairs">
+            <h3>Convite aos keynote speakers</h3>
+            <div>
+              <p>Começando pelos internacionais, que precisam de mais antecedência dada a longa viagem e as agendas concorridas, e então partindo para keynote nacional e fora-da-caixa.</p>
+            </div>
+          </li>
+          <li class="committee">
+            <h3>Definição das trilhas</h3>
+            <div>
+              <p>São as grandes áreas de conhecimento que esperamos ter no programa da Agile Brazil. Conheça as <a href="https://submissoes.agilebrazil.com/2015/tracks" target="_blank">trilhas de 2015</a>!</p>
+
+              <p>Número e títulos de trilhas são rediscutidos todo ano porque servem, também, como forma de incentivar propostas de temas que os membros do comitê percebem como interessantes para o público.</p>
+            </div>
+          </li>
+          <li class="committee">
+            <h3>Definição dos tipos de sessão</h3>
+            <div>
+              <p>São os formatos de sessão que esperamos ver na conferência.</p>
+
+              <p>Muitos deles são <a href="https://submissoes.agilebrazil.com/2015/session_types" target="_blank">usados para submissões</a>: palestra, relato de experiência, mão na massa são exemplos. Outros, como keynote e open spaces, não são submetidos mas farão parte da conferência!</p>
+            </div>
+          </li>
+          <li class="committee">
+            <h3>Definição dos perfis de audiência</h3>
+            <div>
+              <p>Para facilitar a escolha das sessões pelo público da conferência, autores precisam classificar a audiência esperada em níveis, além de descrever os pré-requisitos.</p>
+
+              <p>Em 2015 estamos tentando algo novo: em vez de iniciante, praticante e experiente, <a href="https://submissoes.agilebrazil.com/2015/audience_levels" target="_blank">temos 4 níveis baseados na escala de Dreyfus</a>.</p>
+            </div>
+          </li>
+          <li class="committee">
+            <h3>Definição de regras de submissão</h3>
+            <div>
+              <p>Seja por questões de viabilidade financeira, ou para dar espaço para novos nomes surgirem, também as revisamos todo ano.</p>
+
+              <p>Por exemplo, não permitimos que um mesmo palestrante fale em mais do que duas sessões, nem que um mesmo conjunto de autores se repita em duas sessões. Conheça todas as <a href="https://submissoes.agilebrazil.com/2015/guidelines" target="_blank">regras de 2015</a>!</p>
+            </div>
+          </li>
+          <li class="committee">
+            <h3>Definição do calendário</h3>
+            <div>
+              <p>A definição das datas para cada atividade, desde a abertura de submissão até a publicação da grade no site.</p>
+
+              <p>As datas mostradas no sistema de submissão são apenas algumas delas! Há outros prazos intermediários para garantir que tudo está bem encaminhado!</p>
+            </div>
+          </li>
+          <li class="chairs">
+            <h3>Inscrições de track leads</h3>
+            <div>
+              <p>Com as trilhas definidas, perguntamos na lista do comitê quem gostaria de liderar alguma trilha e os pares são formados. Track 	leads:</p>
+              <p>- não avaliam propostas da sua trilha, assim ele tem voto de minerva se necessário;</p>
+              <p>- não pode submeter para sua trilha, para evitar conflito de interesse;</p>
+            </div>
+          </li>
+          <li class="chairs">
+            <h3>Adequação do sistema de submissões</h3>
+            <div class="">
+              <p>Seja pedindo para o time de desenvolvimento fazer, ou arregaçando as mangas e codando, garantimos que o sistema seja atualizado para o ano corrente.</p>
+            </div>
+          </li>
+          <li class="trackLeads">
+            <h3>Convite aos revisores</h3>
+            <div>
+              <p>Revisando a lista de revisores do ano anterior e elencando novos potenciais revisores da comunidade, montamos a lista de discussão e convidamos essas pessoas.</p>
+            </div>
+          </li>
+        </ul>
+        <h2>Submissões abrem!</h2>
+        <ul>
+          <li class="trackLeads">
+            <h3>Divulgação das submissões</h3>
+            <div>
+              <p>Essa é uma atividade que começa agora e não pára até o final do prazo, se intensificando conforme a data limite se aproxima</p>
+            </div>
+          </li>
+          <li class="reviewers">
+            <h3>Comentários nas submissões</h3>
+            <div>
+              <p>Para ajudar os que submetem antes e os que buscam melhorar suas propostas, os revisores começam seus trabalhos mais cedo, comentando com dúvidas e dicas na maior parte das submissões.</p>
+            </div>
+          </li>
+          <li class="trackLeads">
+            <h3>Acompanhamento de submissões por trilha</h3>
+            <div>
+              <p>Embora trilhas sejam projetadas para movimentar um certo assunto, corre-se o risco de termos poucas submissões em alguma delas. Os track leads devem ficar atentos a isso e fazer divulgações mais específicas, nesse caso!</p>
+            </div>
+          </li>
+          <li class="chairs">
+            <h3>Reforço de divulgação para submissões</h3>
+            <div>
+              <p>Todos os organizadores e diversos membros da comunidade em geral são acionados para ajudarem no esforço final de divulgação do prazo de submissões.</p>
+            </div>
+          </li>
+        </ul>
+        <h2>Aí as submissões fecham.</h2>
+        <ul>
+          <li class="reviewers">
+            <h3>Revisões das propostas</h3>
+            <div>
+              <p>Cada proposta, então, recebe três revisões independentes dos revisores, que consideram cada campo submetido e o perfil dos autores.</p>
+            </div>
+          </li>
+          <li class="trackLeads">
+            <h3>Seleção das sessões</h3>
+            <div>
+              <p>Com todas as revisões feitas, track leads trabalham com seus pares para definir quais sessões querem aprovar, deixar como reserva ou reprovar.</p>
+            </div>
+          </li>
+          <li class="trackLeads">
+            <h3>Consolidação entre trilhas</h3>
+            <div>
+              <p>Todos os track leads se encontram online para verificar restrições e assuntos repetidos, então escolher quais sessões serão escolhidas, quais ficarão como reserva e quais não entrarão.</p>
+            </div>
+          </li>
+          <li class="trackLeads">
+            <h3>Escrita dos feedbacks</h3>
+            <div>
+              <p>Quando decisões de aprovação estão tomadas, os track leads ainda adicionam um breve feedback próprio em cada proposta e, quando estão todos prontos, os e-mails de confirmação são enviados.</p>
+            </div>
+          </li>
+          <li class="chairs">
+            <h3>Confirmação dos autores</h3>
+            <div>
+              <p>Chairs ficam de olho na confirmação dos autores das sessões aceitas e avisam os track leads quando há alguma desistência, para que eles elenquem o substituto.</p>
+            </div>
+          </li>
+          <li class="chairs">
+            <h3>Montagem e publicação da grade</h3>
+            <div>
+              <p>Os chairs de programa, então, colaboram para montar uma grade para o evento que tenha as sessões mais populares em salas maiores e minimizando conflitos de assunto por horário.</p>
+            </div>
+          </li>
+        </ul>
+        <h2>Depois da conferência...</h2>
+        <ul>
+          <li class="chairs">
+            <h3>Consolidação dos feedbacks de sessão</h3>
+            <div>
+              <p>Depois de passar a conferência, os voluntários entregam os feedbacks por sessão para os chairs, que organizam tais dados e tiram estatísticas sobre eles.</p>
+
+              <p>Além disso, aos autores que demonstrarem interesse, enviamos os números de verdes, amarelos e vermelhos recebidos.</p>
+            </div>
+          </li>
+          <li class="chairs">
+            <h3>Cobrar e conferir vídeos gravados</h3>
+            <div>
+              <p>O processo de conseguir os videos gravados durante a conferência é longo: é preciso cobrar pelas gravações, editar os vídeos enviados, adicionar vinheta e subir para <a href="https://vimeo.com/agilebrazil" target="_blank">nosso Vimeo</a> ou na <a href="http://www.agilealliance.org/pt/videos-do-agilebrazil/" target="_blank">biblioteca pública da Agile Alliance</a></p>
+            </div>
+          </li>
+          <li class="committee">
+            <h3>Retrospectiva e sugestões</h3>
+            <div>
+              <p>O comitê todo, então, tem a voz para apontar o que precisa melhorar e propor sugestões, que serão avaliadas pelos próximos chairs e comitê de programa, no início da organização da Agile Brazil seguinte!</p>
+            </div>
+          </li>
+        </ul>
+      </section>
+    );
+
     return (
-      <div>
+      <main>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer program--container">
           <Introduction />
           <WhoWeAre />
+          <TimeLineEvents />
         </div>
-      </div>
+      </main>
     );
   }
 }
