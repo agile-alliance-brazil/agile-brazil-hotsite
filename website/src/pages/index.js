48f874cefe7cf0cc750bb8b1f990415d86d45a0f
Centeno <centeno@diegocenteno.com>
2020-08-20 19:45:45 -0300

    Add new banner to abraca

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -9,11 +9,9 @@ const IndexPage = () => {
     <Layout seoTitle="Inicial" fluid>
       <Jumbotron fluid>
         <Container>
-          <Container>
-            <Image className="align-self-center" src={logo} fluid />
-            <h1>Porto Alegre, 2020</h1>
-            <h2>7, 8 e 9 de Outubro</h2>
-          </Container>
+          <Image className="align-self-center" src={logo} fluid />
+          <h1>Porto Alegre, 2021</h1>
+          <h2>6, 7 e 8 de Outubro</h2>
         </Container>
       </Jumbotron>
     </Layout>
--- website/src/templates/layout.js
+++ website/src/templates/layout.js
@@ -5,17 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../assets/stylesheets/layout.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../assets/stylesheets/layout.css"
 
-import React from "react";
-import PropTypes from "prop-types";
-import { Container } from "react-bootstrap";
+import React from "react"
+import PropTypes from "prop-types"
+import { Jumbotron, Container, Image, Button } from "react-bootstrap"
 
-import Footer from "../components/footer";
-import NavigationBar from "../components/navbar";
-import SEO from "../components/seo";
-import PriceTable from "../components/priceTable/priceTable";
+import Footer from "../components/footer"
+import NavigationBar from "../components/navbar"
+import SEO from "../components/seo"
+import logo from "../assets/images/logo-agile-brazil.png"
 
 const Layout = ({ children, seoTitle, fluid = false }) => {
   return (
@@ -23,18 +23,30 @@ const Layout = ({ children, seoTitle, fluid = false }) => {
       <SEO title={seoTitle} />
       <NavigationBar />
       <Container className="content-container" fluid={fluid}>
+        <Jumbotron className="jumbotron-caravana" fluid>
+          <Container>
+            <Image className="align-self-center" src={logo} fluid />
+            <h1>Agile Brazil Caravana - #ABRaCa</h1>
+            <h2>Slow Conference Online</h2>
+            <h3>08 de Setembro a 07 de Outubro</h3>
+            <Button variant="outline-info" size="lg" href="https://www.even3.com.br/abraca2020/">
+              Saiba mais ...
+            </Button>
+          </Container>
+        </Jumbotron>
+      </Container>
+      <Container className="content-container" fluid={fluid}>
         {children}
       </Container>
-      <PriceTable />
       <Footer />
     </Container>
-  );
-};
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
   seoTitle: PropTypes.string.isRequired,
-  fluid: PropTypes.bool
-};
+  fluid: PropTypes.bool,
+}
 
-export default Layout;
+export default Layout
