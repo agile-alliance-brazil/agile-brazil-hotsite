b459833d08173969538df659e9bb92c07cbedeb1
Centeno <centeno@diegocenteno.com>
2020-08-21 15:07:52 -0300

    Add http redirect to even3

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,21 +1,8 @@
-import React from "react"
-import { Jumbotron, Container, Image } from "react-bootstrap"
-
-import Layout from "../templates/layout"
-import logo from "../assets/images/logo-agile-brazil.png"
-
 const IndexPage = () => {
-  return (
-    <Layout seoTitle="Inicial" fluid>
-      <Jumbotron fluid>
-        <Container>
-          <Image className="align-self-center" src={logo} fluid />
-          <h1>Porto Alegre, 2021</h1>
-          <h2>6, 7 e 8 de Outubro</h2>
-        </Container>
-      </Jumbotron>
-    </Layout>
-  )
+  if (typeof window !== 'undefined') {
+    window.location = 'https://www.even3.com.br/abraca2020/';
+  }
+  return null
 }
 
 export default IndexPage
